cecf616b1d50c1d02d87e292f85bb57dffc39585
RANA AHMAD <74317544+RanaAhmad112@users.noreply.github.com>
2020-12-05 01:41:20 -0800

    3. Commit:Add Two Level to run your app (Simple & Hard). 1
    Add Two Level to run your app (Simple & Hard).

---
+++ flutter_exam1/lib/main1.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'main.dart.';
+import 'Simple.dart';
+import 'Hard.dart';
+
+void main() {
+  runApp(SplashScreen());
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Splash Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+            () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => SimpleHard())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black87,
+      child: Splash(),
+    );
+  }
+}
+//icon Changed
+
+class SimpleHard extends StatefulWidget {
+  @override
+  _SimpleHardState createState() => _SimpleHardState();
+}
+
+class _SimpleHardState extends State<SimpleHard> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Home',
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context) => Simple()));
+                },
+                child: Text(
+                  'Simple',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 40.0,
+                width: 400.0,
+              ),
+              RaisedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                      context, MaterialPageRoute(builder: (context) => Hard()));
+                },
+                child: Text(
+                  'Hard',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
